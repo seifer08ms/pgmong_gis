1f37e38696c622f84c72258b12baf9eddee9c2b0
seifer08ms <hi181904665@gmail.com>
2017-06-10 00:58:39 +0800

    added notebook

---
+++ notebook/app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import os
+from pymongo import MongoClient
+from datetime import datetime
+from random import randint
+
+client =  MongoClient("mongodb://mongo:27017")
+db = client.tododb
+
+
+result = db.restaurants.insert_one(
+    {
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "grades": [
+            {
+                "date": datetime.strptime("2014-10-01", "%Y-%m-%d"),
+                "grade": "A",
+                "score": 11
+            },
+            {
+                "date": datetime.strptime("2014-01-16", "%Y-%m-%d"),
+                "grade": "B",
+                "score": 17
+            }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    }
+)
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
